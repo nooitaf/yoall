c284ae34a29cc07a1c5e76fdc69a37e1f8bde4c4
nooitaf <nooitaf@gmail.com>
2017-06-07 17:29:43 +0200

    less cpu hog, less log, less aggressive fetch

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 
 var TIMER = null
+var TIMER_INTERVAL = 30000
 var DATA_CURRENT = {}
 var DATA_PREPARE = {}
 var MAX_REQUESTED = 10
 
 Meteor.startup(function() {
-  console.log('yobit parser started.')
+  console.log('yobit parser started.' + new Date())
   TIMER = Meteor.setInterval(function(){
   	Meteor.call('fetch')
-	  // Meteor.clearInterval(TIMER)
-  }, 30000)
+  }, TIMER_INTERVAL)
 });
 
 Router.route( "/", function() {
@@ -19,13 +19,12 @@ Router.route( "/", function() {
 
 Meteor.methods({
 	fetch: function(){
-		console.time('fetch')
 		HTTP.get('https://yobit.net/api/3/info', function(error, result){
 			if (!error) {
 				var content = JSON.parse(result.content)
 				// get data calls
 				var pairs = _.keys(content.pairs)
-				console.log(pairs.length + ' calls')
+				// console.log(pairs.length + ' calls')
 				// create string chunks
 				var longpairs = []
 				var longpair = []
@@ -42,21 +41,25 @@ Meteor.methods({
 				}
 
 				DATA_PREPARE = {}
-				console.log(longpairs.length + ' chunk calls')
+				// console.log(longpairs.length + ' chunk calls')
 				_.each(longpairs, function(longstring){
-					HTTP.get('https://yobit.net/api/3/ticker/' + longstring, function(e,r){
-						if (!e){
-							var c = JSON.parse(r.content)
-							_.each(_.keys(c), function(key){
-								DATA_PREPARE[key] = c[key]
-							})
-							if (_.keys(DATA_PREPARE).length === pairs.length) {
-								DATA_CURRENT = DATA_PREPARE
-								console.log('data updated: ', new Date().getTime())
-								console.timeEnd('fetch')
-							}
-						}
-					})
+          // go easy on them and the host
+          Meteor.setTimeout(function(){
+            // console.time('fetch')
+            HTTP.get('https://yobit.net/api/3/ticker/' + longstring, function(e,r){
+              if (!e){
+                // console.timeEnd('fetch')
+                var c = JSON.parse(r.content)
+                _.each(_.keys(c), function(key){
+                  DATA_PREPARE[key] = c[key]
+                })
+                if (_.keys(DATA_PREPARE).length === pairs.length) {
+                  DATA_CURRENT = DATA_PREPARE
+                  console.log('data updated: ', new Date().getTime())
+                }
+              }
+            })
+          }, Math.round(Math.random()*(TIMER_INTERVAL/2)))
 				})
 			}
 		})
---
+++ upload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp -P 22222\
+	/tmp/yoall.tar.gz \
+	root@37.139.24.53:/opt/yoall/build.tar.gz
